4ebca30cfee9901b3a564bff3a22409148ad5d36
Muhamad Iqbal <mirfmaster@gmail.com>
2024-12-20 20:16:00 +0700

    update jobs to three minutes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->job(new GetUserIndofoll)->daily();
-        $schedule->job(new SyncBJSOrders)->everyMinute()->withoutOverlapping();
+        $schedule->job(new SyncBJSOrders)->everyThreeMinutes()->withoutOverlapping();
     }
 
     /**

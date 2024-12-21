80fbc182a49d651a82823c41dff80a8c003644e1
Muhamad Iqbal <mirfmaster@gmail.com>
2024-12-21 09:49:47 +0700

    feat: integrate with template argon 2 dashboard

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,12 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-        $this->call(WorkerSeeder::class);
+        DB::table('users')->insert([
+            'username' => 'admin',
+            'firstname' => 'Admin',
+            'lastname' => 'Admin',
+            'email' => 'djarum76@bjs.com',
+            'password' => bcrypt('interstellarpurgatory'),
+        ]);
     }
 }
